6060184def1b887a3ff77eebb541ee37777175e3
H-N41K <hemendranaik@gmail.com>
2021-06-29 22:02:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
@@ -10,7 +10,7 @@ import Series from "./Pages/Series/Series";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <div className="app">
         <Container>
@@ -24,7 +24,7 @@ function App() {
       </div>
       
       <SimpleBottomNavigation />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
